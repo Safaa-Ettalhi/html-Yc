3741291b78bfd5701c2a908a876f3e16e578a90a
Zouhair Begdar <parkour@ora.ma>
2024-09-30 10:16:37 +0100

    add nav bar to login page

--- app/connexion.js
+++ app/connexion.js
@@ -9,3 +9,13 @@ signUpButton.addEventListener("click", () => {
 signInButton.addEventListener("click", () => {
   container.classList.remove("right-panel-active");
 });
+
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
